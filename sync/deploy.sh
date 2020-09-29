d3027e04666bb948875d669a1ba52cda5511fcdd
Alexander V. Groshev <sattellite@insystem.su>
2020-09-29 13:56:16 +0300

    Added hasing content for ignore empty changes

--- sync/deploy.sh
+++ sync/deploy.sh
@@ -6,11 +6,21 @@ TZ=Europe/Moscow
 cd $(dirname ${BASH_SOURCE[0]})
 # Path of the repository
 ROOT=$(git rev-parse --show-toplevel)
+HASHFILE=.content_sha1
 
 cd ${ROOT}
 
 ${ROOT}/sync/synchronize.sh
 
-if [ $? -eq 0 ]; then
+SYNC_EXIT_CODE=$?
+
+# Read previous hash of content
+touch ${HASHFILE}
+PREVHASH=$(cat ${HASHFILE})
+# Calculate current hash of content
+CURRHASH=$(find $(ls . | grep -v sync) -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
+
+if [ ${SYNC_EXIT_CODE} -eq 0 ] && [ "${PREVHASH}" != "${CURRHASH}" ]; then
   hugo -D
+  echo ${CURRHASH} > ${HASHFILE}
 fi
