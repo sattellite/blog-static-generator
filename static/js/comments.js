923ff01634b4ad2f6317a387fc3b848f173e2e2a
sattellite <null@sattellite.me>
2020-10-25 22:00:21 +0300

    Fixed missprint in remark theme

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,8 +1,11 @@
-
+;
 var remark_config = {
   host: "https://b.sattellite.me/comments",
   site_id: 'sattelliteme',
   locale: window.navigator.language.split('-').pop(),
-  theme: (function() {var m=window.matchMedia("(prefers-color-scheme: dark)"); return m.matches})() ? 'darK': 'light',
+  theme: (function() {var m=window.matchMedia("(prefers-color-scheme: dark)"); return m.matches})() ? 'dark' : 'light',
   show_email_subscription: false
 };
+
+window.matchMedia('(prefers-color-scheme: dark)')
+  .addListener(e => window.REMARK42.changeTheme(e.matches ? 'dark' : 'light'));
