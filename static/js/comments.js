bc2c55cfbf68c715c1aca4a7313eddbae53fd743
sattellite <null@sattellite.me>
2020-10-25 15:52:29 +0300

    Added comments from remark42.com

---
+++ static/js/comments.js
@@ -0,0 +1,6 @@
+var remark_config = {
+  host: "https://b.sattellite.me/comments",
+  site_id: 'sattelliteme',
+  locale: window.navigator.language.split('-').pop(),
+  show_email_subscription: false
+};
