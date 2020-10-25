292c178bd7ecdcd77d9b47cd12497e8cf9cd8295
sattellite <null@sattellite.me>
2020-10-25 16:04:03 +0300

    Styles for comments

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,6 +1,8 @@
+
 var remark_config = {
   host: "https://b.sattellite.me/comments",
   site_id: 'sattelliteme',
   locale: window.navigator.language.split('-').pop(),
+  theme: (function() {var m=window.matchMedia("(prefers-color-scheme: dark)"); return m.matches})() ? 'darK': 'light',
   show_email_subscription: false
 };
